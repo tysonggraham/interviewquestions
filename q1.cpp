d25254d5465db3765d604d4b66d4e92e3fa21fad
tyson <tyson.g.graham>
2016-02-03 22:10:39 -0700

    added 2 solutions to 1st interview question

--- q1.cpp
+++ q1.cpp
@@ -4,38 +4,93 @@
 #include <map>
 using namespace std;
 
+
+
+
+
 int main () 
 {
-	 map<int, int> mapName;
+	 map<int, int> myMap;
 
+	
+	int maxElement = 0;
+	int maxCount = 0;
 	int a[] = {1,2,3,4,5,6,7,7,7,7};
 	for (int i = 9; i >= 0; i--)
 	{
-		//if mapName with key of the value a[i]
-		if (mapName[a[i]])
+		myMap[a[i]]++;
+
+		if(maxCount < myMap[a[i]])
 		{
-			//++ preincrement is more efficient than post
-			++mapName[a[i]];
-		}
-		else
-		{
-			mapName[a[i]] = 1;
-		}
-	}
-	int maxElement = 0;
-	int maxCount = 0;
-	// It doesn't ruin it if you pass by reference so let's do it because it is more efficient
-	// This allows us to change the a array whenever we want though.
-	for (const auto &pair : mapName) 
-	{
-		// cout << p; 
-		// cout << "mapName[" << p.first << "] = " << p.second << '\n'; 
-		if(pair.second > maxCount)
-		{
-			maxCount = pair.second;
-			maxElement = pair.first;
+			maxElement = a[i];
+			maxCount = myMap[a[i]];
 		}
 	}
 	cout << "Hey good lookin' the mode number is: " << maxElement << endl;
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int main () 
+// {
+// 	 map<int, int> myMap;
+
+// 	int a[] = {1,2,3,4,5,6,7,7,7,7};
+// 	for (int i = 9; i >= 0; i--)
+// 	{
+// 		//if myMap with key of the value a[i]
+// 		if (myMap[a[i]])
+// 		{
+// 			//++ preincrement is more efficient than post
+// 			++myMap[a[i]];
+// 		}
+// 		else
+// 		{
+// 			myMap[a[i]] = 1;
+// 		}
+// 	}
+// 	int maxElement = 0;
+// 	int maxCount = 0;
+// 	// It doesn't ruin it if you pass by reference so let's do it because it is more efficient
+// 	// This allows us to change the a array whenever we want though.
+// 	for (const auto &pair : myMap) 
+// 	{
+// 		// cout << p; 
+// 		// cout << "myMap[" << p.first << "] = " << p.second << '\n'; 
+// 		if(pair.second > maxCount)
+// 		{
+// 			maxCount = pair.second;
+// 			maxElement = pair.first;
+// 		}
+// 	}
+// 	cout << "Hey good lookin' the mode number is: " << maxElement << endl;
+// 	return 0;
+// }
